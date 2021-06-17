7e5333b39cbeb1af0f522ad30e6e18a87e23451a
miyad <1505074.msr@ugrad.cse.buet.ac.bd>
2021-06-17 14:40:11 +0600

    calculator type

--- test.js
+++ test.js
@@ -2,9 +2,21 @@ console.log("ai hello world from javascript");
 //alert("Don't Quit! please");
 let a = 0
 document.getElementById("txt").innerHTML = "Changed from JAVASCRIPT file!!";
+
+function appendValue(a){  
+  document.getElementById("txt").innerHTML="function call done! "+a
+
+}
 function myf(){
-  document.getElementById("txt").innerHTML="function call done!"+a;
   a = a + 1;
+  appendValue(a);
   console.log(a);
 }
 
+function foo(){
+  let val = document.getElementById("holder").value;
+  let intVal = parseInt(val,10);
+  a = a + intVal;
+  appendValue(a);
+  console.log(val);
+}
